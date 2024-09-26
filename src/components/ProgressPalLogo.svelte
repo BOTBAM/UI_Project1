66082b1de3a4c3d35ab45bf3f0b356baecc4691e
C0D3GH0ST <167582022+C0D3GH0ST@users.noreply.github.com>
2024-09-25 21:31:04 -0400

    Typo

--- src/components/ProgressPalLogo.svelte
+++ src/components/ProgressPalLogo.svelte
@@ -1,6 +1,6 @@
 <!-- ProgressPalLogo.svelte -->
 <div class="logo">
-  <img src="https://github.com/BOTBAM/UI_Project1/tree/main" alt="ProgressPal">
+  <img src="https://github.com/BOTBAM/UI_Project1/tree/main/ProgressPal3.png" alt="ProgressPal">
 </div>
   
 <style>
